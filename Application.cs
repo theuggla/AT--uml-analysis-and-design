a5bad1a08c07dc3f789f75f537a83cce7c19794c
Molly Arhammar <mollyarhammar@gmail.com>
2017-04-27 14:53:57 +0200

    implement a Card-struct

--- Application.cs
+++ Application.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Text;
-using System.Collections.Generic;
 
 namespace TwentyOneCardGame
 {
@@ -14,10 +12,31 @@ namespace TwentyOneCardGame
         /// </summary>
         public static void Run()
         {
+            Card card = new Card(Suit.Clubs, Rank.Jack);
+            Card card2 = new Card(Suit.Diamonds, Rank.Ace);
+            card2.AceValue = 1;
+            Card[] array = {card, card2};
 
             try
             {
                 Console.WriteLine("Hi! Yes it works.");
+                Console.WriteLine(card);
+                Console.WriteLine(card2);
+                Console.WriteLine("comparing");
+                Console.WriteLine(card.CompareTo(card2));
+                Console.WriteLine(card.Equals(card2));
+                Card[] array2 = (Card[])array.Clone();
+                Console.WriteLine("comparing acevalues");
+                Console.WriteLine(array[1].AceValue);
+                Console.WriteLine(array2[1].AceValue);
+                Console.WriteLine(array[1].Equals(array2[1]));
+                Console.WriteLine("change acevalue");
+                array[1].AceValue = 14;
+                Console.WriteLine("comparing acevalues");
+                Console.WriteLine(array[1].AceValue);
+                Console.WriteLine(array2[1].AceValue);
+                Console.WriteLine(array[1].Equals(array2[1]));
+                
             }
             catch(Exception e)
             {
---
+++ Card.cs
@@ -0,0 +1,99 @@
+using System;
+
+namespace TwentyOneCardGame
+{
+    /// <summary>
+    /// A Card struct with suit, rank and changeble Ace-value.
+    /// </summary>
+    struct Card
+    {
+        int? _aceValue;
+        public readonly Suit Suit;
+        public readonly Rank Rank;
+
+        /// <summary>
+        /// Keeps track of wether the ace is counted as 1 or 14. Returns null if the Rank is not Ace.
+        /// </summary>
+        public int? AceValue 
+        {
+            get
+            {
+                return this._aceValue;
+            }
+            set
+            {
+                if (!(this.Rank == Rank.Ace)) 
+                {
+                    throw new InvalidOperationException("Cannot set AceValue for Card of other Rank than Ace.");
+                } 
+                else if ((value == 1 || value == 14))
+                {
+                    this._aceValue = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Initializes a card witha suit and a rank.
+        /// AceValue will start out as 14 if card is an Ace.
+        /// </summary>
+        public Card(Suit suit, Rank rank)
+        {
+            this._aceValue = null;
+
+            this.Suit = suit;
+            this.Rank = rank;
+
+            if (this.Rank == Rank.Ace)
+            {
+                this.AceValue = (int)this.Rank;
+            }
+        }
+
+        /// <summary>
+        /// Compares two Card's ranks.
+        /// </summary>
+        /// <param name="other"> The card to compare with.</param>
+        ///<returns>1 if value of other Rank is larger than this Rank, 0 if equal, -1 if this is larger than other.</returns>
+        public int CompareTo(Card other)
+        {
+            int otherValue = other.AceValue ?? (int)other.Rank;
+            int thisValue = this.AceValue ?? (int)this.Rank;
+
+            return thisValue.CompareTo(otherValue);
+        }
+
+        ///<summary>
+        ///Returns a string representation of the card.
+        ///</summary>
+        ///<returns>Rank and suit as a string.</returns>
+        public override string ToString()
+        {
+            string rank = (int)this.Rank > 10 ? this.Rank.ToString() : ((int)this.Rank).ToString();
+            char suit = (char)this.Suit;
+
+            return $"{rank[0]}{suit}";
+        }
+
+        ///<summary>
+        ///Checks if this card equals another object.
+        ///</summary>
+        ///<param name="obj"> The object to compare to.</param>
+        ///<returns>True if the other object is a Card with the same suit, rank and acevalue.</returns>
+        public override bool Equals(object obj)
+        {
+             if (obj.GetType().Equals(typeof(Card)))
+             {
+                 Card other = (Card) obj;
+                 
+                 return this.Rank == other.Rank && this.AceValue == other.AceValue && this.Suit == other.Suit;
+             }
+             
+             return false;
+        }
+    }
+}
