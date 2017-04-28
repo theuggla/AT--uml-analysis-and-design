b2c56d1a70caa0459b216ad4ba6ab7d971003490
Molly Arhammar <mollyarhammar@gmail.com>
2017-04-28 16:00:06 +0200

    add run through the game

--- Application.cs
+++ Application.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using System.Collections.Generic;
 
 namespace TwentyOneCardGame
@@ -10,23 +11,58 @@ namespace TwentyOneCardGame
     {
         /// <summary>
         /// Runs the application.
+        /// Plays loops of rounds with five players until
+        /// only one has money left.
         /// </summary>
         public static void Run()
         {
 
             try
             {
-                Player[] players = {
-                    new BettingPlayer("Karlsson")
-                };
-                var game = new GameTable();
-                game.DealInitialCards(players);
+                List<Player> players = new List<Player>();
+                var theGame = new GameTable();
+                int round = 0;
 
-                while (players[0].InPlay)
+                for (int i = 0, limit = 8; i < 5; i += 1, limit += 2) 
                 {
-                    string result = game.PlayRound(players[0]);
-                    Console.WriteLine(result);
+                    players.Add(new BettingPlayer($"Player #{(i + 1)}", limit)); //give them different limits
                 }
+
+                while (players.Count > 1) 
+                {
+                    round++;
+
+                    var originalColor = Console.BackgroundColor;
+                    Console.BackgroundColor = ConsoleColor.Blue;
+                    Console.WriteLine($"Round {round}");
+                    Console.BackgroundColor = originalColor;
+
+                    theGame.DealInitialCards(players);
+
+                    foreach (Player player in players)
+                    {
+                        Console.WriteLine(theGame.PlayRound(player));
+                        Console.WriteLine();
+                    }
+
+                    players = players
+                    .Where(x => x.InPlay)
+                    .ToList();
+
+                    Console.WriteLine();
+                }
+
+                // Print result.
+                if (players.Count > 0)
+                {
+                    Console.WriteLine($"{players.ElementAt(0).Name} with the limit {players.ElementAt(0).Limit} is the last one with any money left!"); 
+                }
+                else
+                {
+                    Console.WriteLine($"No one is left with money.");
+                }
+                   
+                Console.WriteLine($"{round} rounds were played.");
             }
             catch(Exception e)
             {
